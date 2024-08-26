7063fd88ef2257fd03744edfae78872da413d83d
Femto-v <razanforcode@gmail.com>
2024-08-26 22:31:22 +0700

    just cleaning the code, it doesn't matter actually..

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,7 +4,7 @@
 
 <template>
   <!-- for margin only -->
-  <div class="h-screen w-full grid   ">
+  <div class="h-screen w-full grid">
     <!-- setting the grid -->
     <div class="grid  place-content-center">UcupMood</div>
     <div class="h-48 w-full bg-orange-600">
