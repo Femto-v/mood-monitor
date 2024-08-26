29749c1a3c3ce703e650f7e4045adf9c4eada80a
femto <femto@femto.kali>
2024-08-26 22:25:49 +0700

    initiate the block

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,9 +4,12 @@
 
 <template>
   <!-- for margin only -->
-  <div class="h-screen grid place-content-center">
+  <div class="h-screen w-full grid   ">
     <!-- setting the grid -->
-    <div class="grid place-content-center">UcupMood</div>
+    <div class="grid  place-content-center">UcupMood</div>
+    <div class="h-48 w-full bg-orange-600">
+      <div></div>
+    </div>
   </div>
 </template>
 
