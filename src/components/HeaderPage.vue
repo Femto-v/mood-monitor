b3c8b544c5871b79548c3246f8637db6a940f79b
Femto-v <razanforcode@gmail.com>
2024-08-27 20:35:43 +0630

    tring to fix font and logo

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -3,11 +3,11 @@
 
 <template>
      <!-- setting width,height -->
-     <div class="w-full bg-orange-400 fixed">
+     <div class="w-full bg-orange-400 fixed font-mono">
           <!-- setting padding,flex -->
           <div class="p-2 flex justify-between items-center">
-               <div>UcupMood</div>
-               <div>Toggle</div>
+               <img src="/cat.png" alt="UcupMode-logo" class="h-7">
+               <img src="/menus.png" alt="" class="h-7">
           </div>
      </div>
   
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,9 +4,11 @@
 
 <template>
   <!-- for margin only -->
-  <div class="h-screen w-full grid">
+  <div class="h-screen w-full grid place content-center">
     <!-- setting the grid -->
-    <div class="grid  place-content-center">UcupMood</div>
+    <div class="grid text-3xl  place-content-center">UcupMood</div>
+    <div class="grid text-xs place-content-center">"Your" Daily Emotion Tracker Web-App</div>
+
     <div class="h-48 w-full bg-orange-600">
       <div></div>
     </div>
