c6b8e51febee914a61ae9f2a76a3db271fc8e850
Femto-v <razanforcode@gmail.com>
2024-08-28 20:30:15 +0700

    Log In, Sign Up, More button added

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,13 +4,25 @@
 
 <template>
   <!-- for margin only -->
-  <div class="h-screen w-full grid place content-center">
+  <div class="h-screen w-full grid place content-center gap-10">
     <!-- setting the grid -->
-    <div class="grid text-3xl  place-content-center">UcupMood</div>
+    <div class="grid text-3xl  place-content-center font-extrabold">UcupMood</div>
     <div class="grid text-xs place-content-center">"Your" Daily Emotion Tracker Web-App</div>
 
-    <div class="h-48 w-full bg-orange-600">
+    <div class="h-48 w-full bg-orange-100">
       <div></div>
+    </div>
+
+    <div class="flex justify-center gap-5">
+      <button class="p-2 border-2 border-orange-600 rounded-lg">Log In</button>
+      <button class="p-2 border-2 border-orange-600 rounded-lg">Sign Up</button>
+    </div>
+
+    <div class="flex justify-center">
+      <button class="grid place-content-center ">
+        <p>More</p>
+        <v-icon name="bi-arrow-down-circle" class="place-self-center"/>
+      </button>
     </div>
   </div>
 </template>
