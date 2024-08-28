a9ed42d8167415f12686b2d780cabff079867589
Femto-v <razanforcode@gmail.com>
2024-08-28 20:30:54 +0700

    import ohvueicon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// OhVueIcon import
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+// spesific icon can be more than 2 
+import { BiArrowDownCircle } from "oh-vue-icons/icons";
+// addIcons spesific icon can be more than 2 
+addIcons(BiArrowDownCircle);
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// OhVueIcon 
+app.component("v-icon", OhVueIcon);
+
+// mount app
+app.mount('#app')
