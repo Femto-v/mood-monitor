4ce738e6342526d356556c1cff3b005ac750b9af
Femto-v <razanforcode@gmail.com>
2024-08-30 20:14:31 +0700

    cancel infinite animation

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -31,66 +31,7 @@ import "../css/background-animation.css";
         </div>
         <div class="relative h-32 bg-black">
             <div class="absolute w-full h-full flex items-center bg-orange-100">
-                <!-- infinite scroll -->
-                <div class="w-full inline-flex flex-nowrap">
-                    <ul
-                        class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-                    >
-                        <li>
-                            <div class="h-24 w-32 bg-white"></div>
-                        </li>
-                        <li>
-                            <img src="./disney.svg" alt="Disney" />
-                        </li>
-                        <li>
-                            <img src="./airbnb.svg" alt="Airbnb" />
-                        </li>
-                        <li>
-                            <img src="./apple.svg" alt="Apple" />
-                        </li>
-                        <li>
-                            <img src="./spark.svg" alt="Spark" />
-                        </li>
-                        <li>
-                            <img src="./samsung.svg" alt="Samsung" />
-                        </li>
-                        <li>
-                            <img src="./quora.svg" alt="Quora" />
-                        </li>
-                        <li>
-                            <img src="./sass.svg" alt="Sass" />
-                        </li>
-                    </ul>
-                    <ul
-                        class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-                        aria-hidden="true"
-                    >
-                        <li>
-                            <img src="./facebook.svg" alt="Facebook" />
-                        </li>
-                        <li>
-                            <img src="./disney.svg" alt="Disney" />
-                        </li>
-                        <li>
-                            <img src="./airbnb.svg" alt="Airbnb" />
-                        </li>
-                        <li>
-                            <img src="./apple.svg" alt="Apple" />
-                        </li>
-                        <li>
-                            <img src="./spark.svg" alt="Spark" />
-                        </li>
-                        <li>
-                            <img src="./samsung.svg" alt="Samsung" />
-                        </li>
-                        <li>
-                            <img src="./quora.svg" alt="Quora" />
-                        </li>
-                        <li>
-                            <img src="./sass.svg" alt="Sass" />
-                        </li>
-                    </ul>
-                </div>
+                <!-- some pic  -->
             </div>
         </div>
 
