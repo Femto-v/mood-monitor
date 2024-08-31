97f27b8b3875071a4aa2471bebf678eb460c86cb
Femto-v <razanforcode@gmail.com>
2024-08-31 20:48:30 +0700

    change structure file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import HeaderPage from './components/HeaderPage.vue';
-import MainPage from './components/MainPage.vue'
+import HeaderPage from "./components/HeaderPage.vue";
+import MainPage from "./views/home/MainPage.vue";
 </script>
 
 <template>
-  <HeaderPage/>
-  <!-- setting background -->
-  <div class=" font-mono ">
-    <!-- setting margin -->
-    <MainPage msg="Vite + Vue" />
-  </div>
-  
+    <HeaderPage />
+    <!-- setting background -->
+    <div class="font-mono">
+        <!-- setting margin -->
+        <mainPage />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
