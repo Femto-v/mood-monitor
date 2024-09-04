6d795635f99a486ade674538001e61f7963d4de3
Femto-v <razanforcode@gmail.com>
2024-09-04 19:42:31 +0700

    increase z-index at HeaderPage.vue and fix ExplanationPage.vue

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,21 +1,16 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-     <!-- setting width,height -->
-     <div class="w-full bg-orange-400 fixed font-mono">
-          <!-- setting padding,flex -->
-          <div class="p-2 flex justify-between items-center">
-               <img src="/cat.png" alt="UcupMode-logo" class="h-7">
-               <button>
-                    <img src="/menus.png" alt="" class="h-7">
-               </button>
-               
-          </div>
-     </div>
-  
+    <!-- setting width,height -->
+    <div class="w-full bg-orange-400 fixed font-mono z-10">
+        <!-- setting padding,flex -->
+        <div class="p-2 flex justify-between items-center">
+            <img src="/cat.png" alt="UcupMode-logo" class="h-7" />
+            <button>
+                <img src="/menus.png" alt="" class="h-7" />
+            </button>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/ExplanationPage.vue
+++ src/views/ExplanationPage.vue
@@ -3,21 +3,31 @@
 <template>
     <!-- wallpaper -->
     <div class="bg-orange-300">
-        <div class="grid place-content-center p-3 gap-5">
+        <div class="grid p-3 gap-5">
+            <!-- track your emotion daily -->
             <div>
-                <div class="font-bold text-xl">what is UcupMood?</div>
+                <div class="font-bold text-2xl">Track your emotion daily</div>
+                <div class="">At least you know how is your mood changing</div>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe9%2F9e%2Ff6%2Fe99ef6b92b0266c635bf8d1580f7f509.png&f=1&nofb=1&ipt=9ff764a375b7ab3f4242d6e47f44d6f1c5ed698bf2491aa85552b2c33c712bb6&ipo=images"
+                    alt="ok-image"
+                    class="w-full max-w-72 rounded"
+                />
             </div>
             <div>
-                UcupMood consist of "ucup" and "mood". the word ucup doen't mean
-                anything, because it is the name of the owner's cat😸. "mood"
-                refer to a conscious state of mind or predominant emotion.
+                <div class="font-bold text-2xl">Organize your "stress"</div>
+                <div class="">
+                    you can try to review the main reason you are not in the
+                    mood
+                </div>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.tourismtribe.com%2Fwp-content%2Fuploads%2Fbigstock-Time-for-Review-card-with-colo-84652880.jpg&f=1&nofb=1&ipt=94852fd52aa54820f60e57f94c7e7009e6210d163a70f3b14e3a0ab580647d71&ipo=images"
+                    alt="ok-image"
+                    class="w-full max-w-72 rounded"
+                />
             </div>
-            <img
-                src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F6%2F68%2FOrange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg&f=1&nofb=1&ipt=9b136f3e4534b6595c7ee4ab4641b563e6b835420a824a7503123deb973c3f49&ipo=images"
-                alt="ucup-image"
-                class="w-full max-w-72 place-self-center rounded"
-            />
         </div>
+        <div class="grid p-3"></div>
     </div>
 </template>
 
