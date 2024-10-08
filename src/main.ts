4644c97136ab6f537b5608f9e2513ab420d59f1b
Femto-v <razanforcode@gmail.com>
2024-10-08 22:23:22 +0800

    delete review box

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
+// import
+
+// import createApp
 import { createApp } from "vue";
+// general style
 import "./style.css";
+// import App.vue
 import App from "./App.vue";
-import { OhVueIcon, addIcons } from "oh-vue-icons"; // OhVueIcon import
-import { BiArrowDownCircle } from "oh-vue-icons/icons"; // spesific icon can be more than 2
-import { register } from "swiper/element/bundle"; // import swiper
+// OhVueIcon import
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+// spesific icon can be more than 2
+import { BiArrowDownCircle } from "oh-vue-icons/icons";
+// import swiper
+import { register } from "swiper/element/bundle";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-register(); // register swiper
+// register swiper
+register();
+// addIcons spesific icon can be more than 2
+addIcons(BiArrowDownCircle);
+// Components
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
-addIcons(BiArrowDownCircle); // addIcons spesific icon can be more than 2
-
+// declare app as createApp
 const app = createApp(App);
 
-app.component("v-icon", OhVueIcon); // OhVueIcon
-app.mount("#app"); // mount app
+// OhVueIcon
+app.component("v-icon", OhVueIcon);
+
+// mount app
+// app.mount("#app");
+
+app.use(vuetify).mount("#app");
--- src/views/ReviewPage.vue
+++ src/views/ReviewPage.vue
@@ -3,31 +3,10 @@
         <div class="m-3 grid gap-3">
             <div class="font-bold text-2xl">Review</div>
             <!-- item-review -->
-            <div>
+            <div class="grid gap-4">
                 <!-- outer -->
                 <!-- the box start from here -->
-                <div class="p-6 rounded-md shadow-lg bg-orange-300">
-                    <p class="text-xl font-semibold">
-                        "This App really helps my mental health"
-                    </p>
-                    <div class="mt-4">
-                        <div class="flex items-center">
-                            <img
-                                class="h-12 w-12 rounded-full"
-                                src="https://www.profilebakery.com/wp-content/uploads/2023/08/best-ai-headshot-generators.jpeg"
-                                alt="profile photo"
-                            />
-                            <div class="ml-4">
-                                <p class="text-base font-medium text-gray-700">
-                                    John Doe
-                                </p>
-                                <p class="text-sm text-gray-500">
-                                    Software Developer
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div class="grid grid-template-column"></div>
             </div>
         </div>
     </div>
